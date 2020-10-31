043afa495758c4ddb821740cdb115defab7a5407
hartdegen <hartdegen182@gmail.com>
2020-10-31 21:39:14 +0300

    main logic part 6

--- src/index.js
+++ src/index.js
@@ -6,117 +6,127 @@ import onChange from 'on-change'
 import isUrlValid from './urlCheking.js';
 
 const state = {
+  processState: `filling`,
   typedUrlValid: null,
   approvedRssList: {},
 };
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-const watchedState = onChange(state, (path, value, previousValue) => {
-  console.log(111, path)
-  const urlFromRssList = path.split('approvedRssList.')[1];
+
+const form = document.querySelector(`form`);
+const input = form.querySelector(`input`);
+const submitButton = form.querySelector('button');
+
+const watchedState = onChange(state, (path, value) => {
+  const urlFromRssList = path.split(`approvedRssList.`)[1];
   switch (path) {
-    case 'typedUrlValid':
+    case `typedUrlValid`:
       if (value) {
         input.style.border = null;
-        input.value = '';
+        input.value = ``;
       } else {
-        input.style.border = "thick solid red";
+        input.style.border = `thick solid red`;
       }
       break;
     case `approvedRssList.${urlFromRssList}`:
-      console.log(123123123)
-      const renderFeedsPosts = () => {
-        if (!_.isEmpty(state.approvedRssList)) {
-          createFeedsList()
-          createPostsList()
-        }
+      if (!_.isEmpty(state.approvedRssList)) {
+        createFeedsList();
+        createPostsList();
       }
-      setTimeout(renderFeedsPosts, 2000);
+      break;
+    case `processState`:
+      if (value === `sending`) {
+        submitButton.disabled = true;
+      } else {
+        submitButton.disabled = false;
+      }
       break;
     default:
-      console.log(`SOMETHINGS WRONG`)
+      console.log(`SOMETHINGS WRONG`);
       break;
   }
 });
-form.addEventListener('submit', (e) => {
+
+const parseRssData = (dataObj, urlFromInput) => {
+  const parser = new DOMParser();
+  const rssDataDocument = parser.parseFromString(dataObj.data, `text/xml`);
+  const channel = rssDataDocument.querySelector(`channel`);
+  const items = channel.querySelectorAll(`item`);
+  const posts = {};
+  items.forEach((el) => {
+    const pubDate = Date.parse(el.querySelector(`pubDate`).textContent);
+    const title = el.querySelector(`title`).textContent;
+    const link = el.querySelector(`link`).textContent;
+    posts[pubDate] = {
+      title: title,
+      link: link
+    }
+  });
+  watchedState.approvedRssList[urlFromInput] = {
+    title: channel.querySelector(`title`).textContent,
+    description: channel.querySelector(`description`).textContent,
+    posts: posts
+  };
+}
+
+form.addEventListener(`submit`, (e) => {
   e.preventDefault();
-  const urlFromInput = e.target.querySelector('input').value;
-  console.log(222, state.approvedRssList[`${urlFromInput}`])
-  if (isUrlValid(urlFromInput) && state.approvedRssList[`${urlFromInput}`]) {
+  watchedState.processState = 'sending';
+  const urlFromInput = e.target.querySelector(`input`).value;
+  if (isUrlValid(urlFromInput) && !state.approvedRssList[urlFromInput]) {
     watchedState.typedUrlValid = true;
-    const proxy = 'cors-anywhere.herokuapp.com';
+    const proxy = `cors-anywhere.herokuapp.com`;
     axios.get(`https://${proxy}/${urlFromInput}`)
-      .then((obj) => {
-        const parser = new DOMParser();
-        const rssData = parser.parseFromString(obj.data, 'text/xml');
-        const channel = rssData.querySelector(`channel`);
-        const items = channel.querySelectorAll(`item`);
-        const posts = {}
-        items.forEach((el) => {
-          const pubDate = Date.parse(el.querySelector(`pubDate`).textContent);
-          const title = el.querySelector(`title`).textContent;
-          const link = el.querySelector(`link`).textContent;
-          posts[`${pubDate}`] = {
-            title: title,
-            link: link
-          }
-        });
-        watchedState.approvedRssList[`${urlFromInput}`] = {
-          title: channel.querySelector(`title`).textContent,
-          description: channel.querySelector(`description`).textContent,
-          posts: posts
-        };
-        return rssData;
-      })
-      .catch(error => console.log('ERRRRRRRR', error))
+      .then((obj) => parseRssData(obj, urlFromInput))
+      .catch(error => console.log(`ERRRRRRRR`, error))
+      .finally(() => watchedState.processState = `filling`);
   } else {
     watchedState.typedUrlValid = false;
+    watchedState.processState = `filling`
   }
 });
+
 const createFeedsList = () => {
-  const div = document.createElement('div');
-  const h2 = document.createElement('h2');
+  const div = document.createElement(`div`);
+  const h2 = document.createElement(`h2`);
   h2.textContent = `Feeds`;
   div.appendChild(h2);
-  const ul = document.createElement('ul');
+  const ul = document.createElement(`ul`);
   for (let url in state.approvedRssList) {
-    const title = state.approvedRssList[`${url}`].title;
-    const description = state.approvedRssList[`${url}`].description;
-    const h3 = document.createElement('h3');
-    const p = document.createElement('p');
+    const title = state.approvedRssList[url].title;
+    const description = state.approvedRssList[url].description;
+    const h3 = document.createElement(`h3`);
+    const p = document.createElement(`p`);
     h3.textContent = title;
     p.textContent = description;
-    const li = document.createElement('li');
+    const li = document.createElement(`li`);
     li.appendChild(h3);
     li.appendChild(p);
     ul.prepend(li);
   }
   div.append(ul)
-  const feeds = document.querySelector('.feeds');
+  const feeds = document.querySelector(`.feeds`);
   feeds.innerHTML = div.innerHTML
-}
+};
+
 const createPostsList = () => {
-  const div = document.createElement('div');
-  const h2 = document.createElement('h2');
+  const div = document.createElement(`div`);
+  const h2 = document.createElement(`h2`);
   h2.textContent = `Posts`;
   div.appendChild(h2);
   const urls = _.keys(state.approvedRssList);
   const lastAddedUrl = urls[urls.length - 1];
-  const newestPosts = state.approvedRssList[`${lastAddedUrl}`].posts;
-  const ul = document.createElement('ul');
+  const newestPosts = state.approvedRssList[lastAddedUrl].posts;
+  const ul = document.createElement(`ul`);
   for (let post in newestPosts) {
-    const title = newestPosts[`${post}`].title;
-    const link = newestPosts[`${post}`].link;
-    const a = document.createElement('a');
+    const title = newestPosts[post].title;
+    const link = newestPosts[post].link;
+    const a = document.createElement(`a`);
     a.href = link;
     a.textContent = title;
-    const li = document.createElement('li');
+    const li = document.createElement(`li`);
     li.appendChild(a);
     ul.appendChild(li);
   }
   div.append(ul);
-  const posts = document.querySelector('.posts');
+  const posts = document.querySelector(`.posts`);
   posts.innerHTML = div.innerHTML
-}
-
-export default (a, b) => a + b;
+};
