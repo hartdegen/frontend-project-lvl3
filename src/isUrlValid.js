143b7d47d6e9b1810e4e6855938ef5a480a9aac4
hartdegen <hartdegen182@gmail.com>
2020-12-07 06:29:08 +0300

    review №5 part 9

--- src/isUrlValid.js
+++ src/isUrlValid.js
@@ -1,23 +1,15 @@
 import * as yup from 'yup';
 
-const isUrlValid = (url) => {
-  const schema1 = yup.object().shape({
+export const isValid = (url) => {
+  const schema = yup.object().shape({
     website: yup.string().url(),
   });
-  return schema1.isValidSync({
+  return schema.isValidSync({
     website: url,
   });
 };
 
-export default (url, fullUrlsList, watcher) => {
-  const watchedState = watcher;
-  if (!isUrlValid(url)) {
-    watchedState.form.status = 'urlNotValid';
-    return false;
-  }
-  if (fullUrlsList.includes(url)) {
-    watchedState.form.status = 'alreadyExists';
-    return false;
-  }
-  return true;
+export const isIncluded = (url, list) => {
+  const schema = yup.string().notOneOf(list);
+  return schema.isValidSync(url);
 };
--- src/watcher.js
+++ src/watcher.js
@@ -2,7 +2,8 @@ import _ from 'lodash';
 import i18next from 'i18next';
 import onChange from 'on-change';
 
-const renderFeedsList = (feeds) => {
+const renderFeedsList = (feeds, elem) => {
+  const feedElement = elem;
   const div = document.createElement('div');
   const h2 = document.createElement('h2');
   h2.textContent = 'Feeds';
@@ -19,11 +20,11 @@ const renderFeedsList = (feeds) => {
     ul.prepend(li);
   });
   div.append(ul);
-  const feedsElement = document.querySelector('div.feeds');
-  feedsElement.innerHTML = div.innerHTML;
+  feedElement.innerHTML = div.innerHTML;
 };
 
-const renderPostsList = (rawPosts) => {
+const renderPostsList = (rawPosts, elem) => {
+  const postsElement = elem;
   const posts = rawPosts.reduce((acc, val) => {
     const postsFromUrl = _.values(val.byDate);
     return [...postsFromUrl, ...acc];
@@ -42,29 +43,36 @@ const renderPostsList = (rawPosts) => {
     ul.appendChild(li);
   });
   div.append(ul);
-  const postsElement = document.querySelector('div.posts');
   postsElement.innerHTML = div.innerHTML;
 };
-const renderLoadingStatus = (text) => {
-  document.querySelector('div.loadingInfo').textContent = text;
+
+const renderLoadingStatus = (text, elem) => {
+  const loadingElemenet = elem;
+  loadingElemenet.textContent = text;
 };
 
-export default (state, elements) => {
-  const loadingProcessHandler = (status, elems, watcher) => {
+export default (state, elems) => {
+  const loadingProcessHandler = (status, timerId, initialState) => {
+    const watchedState = initialState;
     switch (status) {
       case 'loading':
+        watchedState.form.status = 'loading';
+        clearTimeout(timerId);
         break;
       case 'failed':
+        watchedState.form.status = 'loading';
+        clearTimeout(timerId);
         break;
       case 'succeed':
+        watchedState.form.status = 'succeed';
         break;
       default:
         throw new Error(`Unknown status: ${status}`);
     }
   };
 
-  const formHandler = (status, elems) => {
-    const { input, submitButton } = elems;
+  const formHandler = (status) => {
+    const { loadingElem, input, submitButton } = elems;
     input.style.border = null;
     switch (status) {
       case 'loading':
@@ -73,19 +81,19 @@ export default (state, elements) => {
       case 'urlNotValid':
         submitButton.disabled = false;
         input.style.border = 'thick solid red';
-        renderLoadingStatus(i18next.t('urlNotValid'));
+        renderLoadingStatus(i18next.t('urlNotValid'), loadingElem);
         break;
       case 'alreadyExists':
         submitButton.disabled = false;
-        renderLoadingStatus(i18next.t('alreadyExists'));
+        renderLoadingStatus(i18next.t('alreadyExists'), loadingElem);
         break;
       case 'failed':
         submitButton.disabled = false;
-        renderLoadingStatus(i18next.t('failed'));
+        renderLoadingStatus(i18next.t('failed'), loadingElem);
         break;
       case 'succeed':
         submitButton.disabled = false;
-        renderLoadingStatus(i18next.t('succeed'));
+        renderLoadingStatus(i18next.t('succeed'), loadingElem);
         break;
       default:
         throw new Error(`Unknown status: ${status}`);
@@ -93,18 +101,22 @@ export default (state, elements) => {
   };
 
   const watchedState = onChange(state, (path, value) => {
+    const { timerId } = state;
+    const { feedsElem, postsElem } = elems;
     switch (path) {
       case 'feeds':
-        renderFeedsList(value);
+        renderFeedsList(value, feedsElem);
         break;
       case 'posts':
-        renderPostsList(value);
+        renderPostsList(value, postsElem);
+        break;
+      case 'timerId':
         break;
       case 'loadingProcess.status':
-        loadingProcessHandler(value, elements);
+        loadingProcessHandler(value, timerId, watchedState);
         break;
       case 'form.status':
-        formHandler(value, elements);
+        formHandler(value);
         break;
       case 'loadingProcess.error':
         throw new Error(`Loading Process: ${value}`);
